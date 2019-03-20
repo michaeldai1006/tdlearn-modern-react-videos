be2b4be4e12c0133e05c0b9fd178f8450d473e12
michaeldai1006 <dtc74110@gmail.com>
2019-03-20 00:22:58 -0700

    Searching for Videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyC7Y2r4YJIEXOGs-UMJjsJRP3-qJjQXBXQ';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
